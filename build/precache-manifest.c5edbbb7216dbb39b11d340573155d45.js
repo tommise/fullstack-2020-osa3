65325486d585f9697d58ed211a4971f2b6b9ef06
Tommi Ek <tommiek83@gmail.com>
2020-02-03 17:28:17 +0200

    Frontend integration

---
+++ build/precache-manifest.c5edbbb7216dbb39b11d340573155d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2743d6b4f8ada4edd5fcb2378694d7f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f5ecec7afc8fded0b3",
+    "url": "/static/css/main.8fc0c20d.chunk.css"
+  },
+  {
+    "revision": "bb2ecaa908522ab39e71",
+    "url": "/static/js/2.98e8e06b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.98e8e06b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70f5ecec7afc8fded0b3",
+    "url": "/static/js/main.0dc1a1b7.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
